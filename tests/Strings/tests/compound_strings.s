4d379de698c9799b82b48a4943e489de3d11e702
Jeroen Venema <jeroen.venema@gmail.com>
2024-03-01 20:50:20 +0100

    Compound string test with binary match

---
+++ tests/Strings/tests/compound_strings.s
@@ -0,0 +1,7 @@
+    db "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+{}[]\\|;:<>,./?`~ "
+    db "\a\b\e\f\n\r\t\v\\\'\"\?"
+    db "String",0
+    defb "String",0
+    ascii "String",0
+    byte "String",0
+    asciz "String"
