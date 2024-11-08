39b9ca862598807cb4260bedd6cdde410ecc089d
Jeroen Venema <jeroen.venema@gmail.com>
2024-11-08 08:33:48 +0100

    Provides automated testcase for issue #88

--- tests/Conditional_asm/tests/basic_ifelse.s
+++ tests/Conditional_asm/tests/basic_ifelse.s
@@ -1,8 +1,27 @@
-TEST: EQU 1
+; Test both if/else code blocks
+; Also test both basic assembly emition and emition of data/blk directives
 
-.if TEST
+TRUE:   EQU 1
+FALSE:  EQU 0
+
+; Test if statement
+.if TRUE
     ld a,1
+    .db 1
+    .blkb 1,1
 .else
     ld a,2
+    .db 2
+    .blkb 1,2
 .endif
 
+; Test else statement
+.if FALSE
+    ld a,3
+    .db 3
+    .blkb 1,3
+.else
+    ld a,4
+    .db 4
+    .blkb 1,4
+.endif
