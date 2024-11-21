3fc338ded6c677e277a0364387f9e76be09c77c1
Jeroen Venema <jeroen.venema@gmail.com>
2024-11-21 09:43:06 +0100

    Testcase for else statement #96

--- tests/Conditional_asm/tests/test.s
+++ tests/Conditional_asm/tests/test.s
@@ -1,7 +1,8 @@
-select: .equ 1
+true: .equ 1
+false: .equ 0
 
 proc:
-.if select           ; this block will be selected as 'select' is 1 
+.if true             ; this block will be selected
     ld hl, localvar  ; loading the address of the label in this block
     ld a, (hl)       ; A will be loaded with 1
     ret
@@ -11,9 +12,25 @@ localvar: .db 1
     ld hl, localvar
     ld a, (hl)
     ret
-    .asciz "Dummy data" ; This shouldn't produce a trailing zero
-variable:            ; This shouldn't error
-localvar: .db 0      ; the same label can be used, because this entire block will be skipped
+    .asciz "This should not be here"
+variable:
+localvar: .db 0     ; the same label can be used, because this entire block will be skipped
+.endif
+
+procfalse:
+.if false           ; this block will NOT be selected
+    ld hl, localvar2; loading the address of the label in this block
+    ld a, (hl)      ; A will be loaded with 1
+    ret
+variable2:
+localvar2: .db 1
+.else
+    ld hl, localvar2
+    ld a, (hl)
+    ret
+    .asciz "This should be here"
+variable2:
+localvar2: .db 0      ; the same label can be used, because this entire block will be skipped
 .endif
 
     ld b, a         ; first regular statement
